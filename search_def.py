27cfdabc9fb9dbcc0d6a749c67e5c54b98fdddfc
Puneet Jain <mailpuneet8264@gmail.com>
2017-12-12 22:14:36 +0530

    More Error handling

--- search_def.py
+++ search_def.py
@@ -1,13 +1,22 @@
 #!/usr/local/opt/python/libexec/bin/python
 
 import json
+from difflib import get_close_matches
 
-data = json.load(open("/Users/puneetjain/Downloads/data.json","r"))
+data = json.load(open("data.json","r"))
 
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys(),cutoff=0.8)) > 0:
+        yes_no = raw_input("Did you mean %s instead? Enter Y if Yes or N if No." % (get_close_matches(w, data.keys(),cutoff=0.8)[0]))
+        if yes_no == "Y" or yes_no == "y":
+            return data[get_close_matches(w, data.keys(),cutoff=0.8)[0]]
+        elif yes_no == "N" or yes_no == "n":
+            return "Word Doesnot Exits!!"
+        else:
+            return "We didn't understand your entry. :("
     else:
         return "Not a Word in the dictionary. Please double check it."
 
